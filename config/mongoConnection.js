db08727605924a071d470cc6c08603b0793cfb04
hanfanw <hanfanw@gmail.com>
2018-11-26 17:38:51 -0500

    uploading mongo

---
+++ config/mongoConnection.js
@@ -0,0 +1,25 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const settings = {
+  mongoConfig: {
+    serverUrl: "mongodb://localhost:27017/",
+    database: "Market-IT"
+  }
+};
+
+let fullMongoUrl =
+  settings.mongoConfig.serverUrl + settings.mongoConfig.database;
+let _connection = undefined;
+let _db = undefined;
+
+
+let connectDb = async () => {
+  if (!_connection) {
+    _connection = await MongoClient.connect(settings.mongoConfig.serverUrl);
+    _db = await _connection.db(settings.mongoConfig.database);
+  }
+
+  return _db;
+};
+
+module.exports = connectDb;
