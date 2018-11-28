831a67c0d1613ae33d7229f1e2181117aae95fbd
hanfanw <hanfanw@gmail.com>
2018-11-28 13:55:33 -0500

    add products page

--- routes/products.js
+++ routes/products.js
@@ -2,4 +2,12 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+router.get("/", async (req, res) => {
+    try {
+      res.render("products");
+    } catch (e) {
+      res.status(500).json({ error: e });
+    }
+  });
+
 module.exports = router;
