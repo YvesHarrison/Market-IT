be752c190bf24dd891e09c097ffba8ec05c23518
Sanchita Rastogi <sanchitarastogi03@gmail.com>
2018-11-28 16:34:31 -0500

    passport authentication

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+
 router.get("/", async (req, res) => {
   try {
     res.render("dashboard");
@@ -9,21 +10,51 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: e });
   }
 });
-
+/*-----------Login and Authentication-------------------------*/
 router.get("/login", async (req, res) => {
   try {
-    res.render("login");
+    res.render("login", { message: req.flash('loginMessage') });
   } catch (e) {
     res.status(500).json({ error: e });
   }
 });
 
+function isLoggedIn(req, res, next) {
+
+  // if user is authenticated in the session, carry on 
+  if (req.isAuthenticated())
+      return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect('/');
+}
+
+app.post('/login', passport.authenticate('local-login', {
+  successRedirect : '/profile', 
+  failureRedirect : '/login', 
+  failureFlash : true 
+}));
+
+/*---------------------------Registaration and Authentication----------------------*/
 router.get("/signup", async (req, res) => {
   try {
-    res.render("signup");
+    res.render("signup", { message: req.flash('signupMessage') });
   } catch (e) {
     res.status(500).json({ error: e });
   }
 });
+
+app.post('/signup', passport.authenticate('local-signup', {
+  successRedirect : '/profile', // redirect to the secure profile section
+  failureRedirect : '/signup', // redirect back to the signup page if there is an error
+  failureFlash : true // allow flash messages
+}));
+
+/*------------------------Logout------------------------------*/
+router.all("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/login");
+});
+
 
 module.exports = router;
