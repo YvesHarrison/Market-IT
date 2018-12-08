a14e76145bb17b3f00e108c944b41d6554a50026
sheeryachavan <shreesh.chavan@gmail.com>
2018-12-08 15:18:41 -0500

    sign-up css

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const bodyParser = require("body-parser");
 const static = express.static(__dirname + "/public");
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash    = require('connect-flash');
-var morgan       = require('morgan');
+var flash = require('connect-flash');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
-var session      = require('express-session');
+var session = require('express-session');
 
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
@@ -23,20 +23,32 @@ const configDB = require('./config/mongoConnection');
 const pass = require('./data/passport')(passport);
 app.use("/public", static);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); 
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch',
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(passport.initialize());
-app.use(passport.session()); 
+app.use(passport.session());
 app.use(flash());
 
 configRoutes(app);
 
 app.listen(3000, () => {
-	console.log("Your server is now listening on port 3000! Navigate to http://localhost:3000 to access it");
-    if (process && process.send) process.send({done: true}); // ADD THIS LINE
+    console.log("Your server is now listening on port 3000! Navigate to http://localhost:3000 to access it");
+    if (process && process.send) process.send({
+        done: true
+    }); // ADD THIS LINE
 });
